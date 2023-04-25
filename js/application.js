fd533669699d87c5a67e881c0b305a6c9bf1dfee
Laura Purcell-Gates <l.purcellgates@gmail.com>
2023-04-25 13:20:57 +0100

    total price functions
    not yet working

--- js/application.js
+++ js/application.js
@@ -1,4 +1,28 @@
+// function to calculate item total
+var calculatePrice = function (price, qty) {
+  return price * qty;
+};
+
+// doc ready function
 $(document).ready(function () {
+  
+  // create array to store item totals
+  var allPrices = [];
+
+  // iterate over rows to capture item totals
+  $('tbody tr').each(function (i, ele) {
+    var price = parseFloat($(ele).children('.price').val());
+    var qty = parseFloat($(ele).find('.quantity input').val());
+    var itemTotal = calculatePrice(price, qty);
+    allPrices.push(itemTotal);
+  });
+
+  // calculate cart total
+  var cartTotal = allPrices.reduce(sum);
+  // insert into html
+  $('#cartTotal').html(cartTotal);
+
+  // functionality of cancel buttons
   $('.btn.cancel').on('click', function (event) {
     $(this).closest('tr').remove();
   });
